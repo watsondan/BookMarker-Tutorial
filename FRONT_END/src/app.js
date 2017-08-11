853669b918c84f8ee81596fcbd693c7117621012
watsondan <watsondan@gmail.com>
2017-08-11 15:42:04 -0500

    More setup stuff.

--- FRONT_END/src/app.js
+++ FRONT_END/src/app.js
@@ -1,4 +1,4 @@
-//<!--app.js-->
+// app.js
 
 import React from 'react'
 import ReactDOM from 'react-dom'
--- FRONT_END/webpack.config.js
+++ FRONT_END/webpack.config.js
@@ -1,4 +1,4 @@
-//webpack.config.js
+// webpack.config.js
 
 const path = require('path')
 
